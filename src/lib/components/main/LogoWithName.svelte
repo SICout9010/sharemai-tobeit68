f23a272b47dab4b8e9cf3a3ccac68678870aefc7
SICout9010 <fivrewgsd@gmail.com>
2024-11-15 15:00:36 +0700

    Fix Landing page

--- src/lib/components/main/LogoWithName.svelte
+++ src/lib/components/main/LogoWithName.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-    import hostmyphotos_logo from "$lib/assets/hostmyphotos_logo.png";
+    import logo from "$lib/assets/Logo.png";
+  import { cn } from "$lib/utils";
+
+    let { class: classname }: { class?: string | null | undefined } = $props();
 </script>
 
-<img src={hostmyphotos_logo} width="50" height="50" alt="logo" class="absolute scale-150"> 
+<img src={logo} width="50" height="50" alt="logo" class={cn("absolute scale-75 drop-shadow-lg", classname)}> 
 <a href="/" class="ml-12 self-center text-xl font-bold">Sharemai</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
+    import Separator from "$lib/components/ui/separator/separator.svelte";
+    import LogoWithName from "$lib/components/main/LogoWithName.svelte";
+    import rb_1209 from "$lib/assets/rb_1209.png";
+    // Sponsor logos
+    import Camphub from "$lib/assets/Camphub.png";
+    import TopCharoen from "$lib/assets/Topcharoen.png";
+    import Major from "$lib/assets/major.jpg";
+    import Aona from "$lib/assets/AONA.jpg";
+    import Lactasoy from "$lib/assets/Lactasoy.jpg";
 </script>
 
 <style>
@@ -11,9 +20,10 @@
 
 <header class="flex justify-between items-center px-8 py-4 bg-transparent text-white">
   <!-- โลโก้ทางซ้าย -->
-  <div class="text-2xl font-bold text-white">
+   <LogoWithName class="scale-75" />
+  <!-- <div class="text-2xl font-bold text-white">
     ShareMai
-  </div>
+  </div> -->
   
   <!-- ปุ่ม Sign Up และ Login ทางขวา -->
   <div class="flex space-x-4">
@@ -22,21 +32,34 @@
   </div>
 </header>
 
-<main class="flex items-center justify-center w-full h-screen text-white">
+<main class="flex flex-col items-center justify-center w-full min-h-screen text-white font-[prompt]">
     <!-- เนื้อหาทางซ้าย -->
-    <section id="hero-left" class="bg-transparent text-white py-24 px-12 rounded-lg shadow-xl max-w-7xl mx-auto flex items-center gap-24 justify-between w-full h-full">
+    <section id="hero-left" class="bg-transparent text-white py-12 px-12 rounded-lg max-w-7xl mx-auto flex items-center gap-24 justify-between w-full h-full">
       <!-- เนื้อหาทางซ้าย -->
       <div class="text-left max-w-xl">
-        <h1 class="text-6xl font-bold mb-6">Traveling solo? Find a buddy and share the journey here!</h1>
+        <h1 class="text-6xl font-bold mb-6 font-[montserrat]">Traveling solo? Find a buddy and share the journey here!</h1>
         <p class="text-lg mb-8">แอปจับคู่หารค่ารถช่วยผู้ใช้ร่วมเดินทาง ลดค่าโดยสาร ประหยัดพลังงาน และลดการปล่อยก๊าซเรือนกระจก</p>
-        <button class="bg-[#1E2031] text-white font-semibold px-8 py-3 rounded-xl hover:shadow-lg">หาเพื่อนร่วมทางเลยตอนนี้</button>
+        <Button class="bg-[#1E2031] text-white font-semibold px-8 py-3 rounded-xl hover:shadow-lg">หาเพื่อนร่วมทางเลยตอนนี้</Button>
       </div>
       
       <!-- รูปภาพทางขวา -->
       <div class="hidden md:block max-w-md">
-        <img src="Group14.png" alt="ภาพที่เกี่ยวข้องกับการเดินทาง" class="rounded-lg shadow-lg w-full h-full object-cover">
+        <img src={rb_1209} alt="ภาพที่เกี่ยวข้องกับการเดินทาง" class="rounded-lg w-full h-full object-cover scale-150">
       </div>
     </section>
+    <!-- Sponsors -->
+    <Separator class="my-4" />
+    <section class="w-full h-[20vh] flex flex-col items-center justify-center gap-4">
+      <p class="text-primary font-bold text-2xl">ขอบคุณผู้ใหญ่ใจดี</p>
+      <div class="flex items-center justify-center gap-8">
+        <img src={Camphub} alt="sponsor" class="w-24 h-24 object-contain">
+        <img src={TopCharoen} alt="sponsor" class="w-24 h-24 object-contain">
+        <img src={Major} alt="sponsor" class="w-24 h-24 object-contain">
+        <img src={Aona} alt="sponsor" class="w-24 h-24 object-contain">
+        <img src={Lactasoy} alt="sponsor" class="w-24 h-24 object-contain">
+      </div>
+    </section>
+    <Separator class="my-4" />
   </main>
   
   
